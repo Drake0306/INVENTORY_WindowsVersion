89507ea2256eb0632fe754deca19d7aa38830dfa
danro <danro@MACBOOKAIR>
2024-08-06 12:16:34 +0530

    Add project files.

---
+++ Activation/IActivationHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace INVENTORY.Activation;
+
+public interface IActivationHandler
+{
+    bool CanHandle(object args);
+
+    Task HandleAsync(object args);
+}
---
+++ Contracts/Services/IPageService.cs
@@ -0,0 +1,6 @@
+﻿namespace INVENTORY.Contracts.Services;
+
+public interface IPageService
+{
+    Type GetPageType(string key);
+}
